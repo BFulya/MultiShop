d3623433467946a944d55c4dac260aea1971c5f4
BasakFulya <basakfulyaytu@gmail.com>
2026-04-10 02:39:01 +0300

    Catalog APIs written(CategoriesController,ProductsController,ProductDetailsController,ProductImages Controller)

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Services.ProductServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _ProductService;
+        public ProductsController(IProductService ProductService)
+        {
+            _ProductService = ProductService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _ProductService.GetAllProductAsync();
+            return Ok(values);
+        }
+        [HttpGet("id")]
+        public async Task<IActionResult> GetProductyById(string id)
+        {
+            var values = _ProductService.GetByIdProductAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductProduct(CreateProductDto createProductDto)
+        {
+            await _ProductService.CreateProductAsync(createProductDto);
+            return Ok("Product added successfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _ProductService.DeleteProductAsync(id);
+            return Ok("Product deleted successfully");
+        }
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _ProductService.UpdateProductAsync(updateProductDto);
+            return Ok("Product updated successfully");
+        }
+    }
+}
